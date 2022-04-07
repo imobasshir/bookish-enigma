82563c974460cc7ea124ca934379f587308cc40a
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-08 00:02:52 +0530

    done

---
+++ src/trainingDay2/FBC.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+package trainingDay2;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class FCB
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int tC = sc.nextInt();
+		while(tC > 0) {
+		    int k = sc.nextInt();
+		    int x = sc.nextInt();
+		    int ans = k - x;
+		    System.out.println(ans);
+		    tC--;
+		}
+	}
+}
---
+++ src/trainingDay2/SNAPE.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+package trainingDay2;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SNAPE
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int tC = sc.nextInt();
+		while(tC > 0) {
+		    int b = sc.nextInt();
+		    int ls = sc.nextInt();
+		    double ansMax = Math.round(Math.sqrt(b*b + ls*ls)*10000.0)/10000.0;
+		    double ansMin = Math.round(Math.sqrt(ls*ls - b*b)*10000.0)/10000.0;
+		    System.out.println(ansMin+ " "+ansMax);
+		    tC--;
+		}
+	}
+}
