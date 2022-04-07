582aa454c7c8f17bf64e70bc7fa297fe44027248
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-07 12:07:49 +0530

    second commit

---
+++ src/trainingDay2/REPEAT.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+package trainingDay2;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class REPEAT
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int tC = sc.nextInt();
+		while(tC > 0) {
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    int s = sc.nextInt();
+		    int ans = (s - (n*n)) / (k-1);
+		    System.out.println(ans);
+		    tC--;
+		}
+	}
+}
