244d1093952d75e05b128ca114c0d7c2d8a5093f
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-07 12:41:26 +0530

    done

---
+++ src/trainingDay2/TRAVELPS.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+package trainingDay2;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class TRAVELPS
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int tC = sc.nextInt();
+		while(tC > 0) {
+		    int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc. nextInt();
+            String str = sc.next();
+            int zero = 0;
+            int ones = 0;
+            for (int i=0; i<n; i++) {
+                if(str.charAt(i) == '1') 
+                    ones++;
+                else
+                    zero++;
+                
+            } 
+            System.out.println(zero*a + ones*b);
+		    tC--;
+		}
+	}
+}
