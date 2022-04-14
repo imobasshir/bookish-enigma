bd9b266d44c60a9e07d23441f2fe40a9e0dce230
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-15 00:53:30 +0530

    zoz problem

---
+++ src/day4/ZOZ.java
@@ -0,0 +1,32 @@
+package day4; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ZOZ
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int tc= sc.nextInt();
+		while(tc>0){
+		    int n = sc.nextInt();
+		    long k = sc.nextInt();
+		    int[] arr = new int[n];
+		    long sum = 0;
+		    for(int i = 0; i<arr.length; i++){
+		        arr[i] = sc.nextInt();
+		        sum += arr[i];
+		    }
+		    int count = 0;
+		    for(int a: arr){
+		        if(sum-a< (long)(a+k))count++;
+		    }
+		    System.out.println(count);
+                    tc--;
+		}
+	}
+}
