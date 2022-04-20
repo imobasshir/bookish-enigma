7ba99f6327344df7597df2cf639083ce750267db
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-04-21 01:12:06 +0530

    equal sum

--- src/day9/EqualSumPart.java
+++ src/day9/EqualSumPart.java
@@ -8,7 +8,35 @@ public class EqualSumPart {
     }
 
     private static boolean equalPart(int[] arr, int n) {
-
-        return false;
+        int Sum = 0;
+        for (int i = 0; i < n; i++) {
+            Sum += arr[i];
+        }
+        if (Sum % 2 != 0) {
+            return false;
+        }
+        int sum = Sum / 2;
+        boolean[][] T = new boolean[n + 1][sum + 1];
+        // initialization condition
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                if (i == 0) {
+                    T[i][j] = false;
+                }
+                if (j == 0) {
+                    T[i][j] = true;
+                }
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    T[i][j] = (T[i - 1][j - arr[i - 1]] || T[i - 1][j]);
+                } else {
+                    T[i][j] = T[i - 1][j];
+                }
+            }
+        }
+        return T[n][sum];
     }
 }
